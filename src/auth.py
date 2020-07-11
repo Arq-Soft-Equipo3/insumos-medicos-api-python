6314acb66b95d10b995332f24effc300221947aa
Inés Sosa <ines.-sosa@hotmail.com>
2020-07-10 23:21:28 -0300

    added role to Payload

--- src/auth.py
+++ src/auth.py
@@ -12,12 +12,13 @@ class AuthManager:
         True
 
     @staticmethod
-    def encodeAuthToken(userEmail):
+    def encodeAuthToken(userEmail, userRole):
         try:
             payload = {
                 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, hours=5),
                 'iat': datetime.datetime.utcnow(),
-                'sub': userEmail
+                'sub': userEmail,
+                'role': userRole
             }
             return jwt.encode(
                 payload,
@@ -36,13 +37,14 @@ class AuthManager:
         except jwt.ExpiredSignatureError:
             raise AuthorizationFailed('Token has expired. Please log in again.')
         except jwt.InvalidTokenError:
-            raise  AuthorizationFailed('Invalid token. Please log in again.')
+            raise AuthorizationFailed('Invalid token. Please log in again.')
 
     def authenticate(self, user, aPassword):
         userPassword = user.get('password').get('B')
         userEmail = user.get('email').get('S')
+        userRole = user.get('role').get('S')
 
         if bcrypt.checkpw(aPassword.encode('utf8'), userPassword):
-            return self.encodeAuthToken(userEmail).decode()
+            return self.encodeAuthToken(userEmail, userRole).decode()
         else:
             raise AuthorizationFailed('Invalid credentials. Try again')
